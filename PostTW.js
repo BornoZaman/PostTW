1140e21618a9cc2c17b7817a242004040969ee7e
Borno Zaman <borno.zaman@gmail.com>
2015-10-18 22:25:58 -0400

    Basic functionality done (Too many changes)

--- PostTW.js
+++ PostTW.js
@@ -9,6 +9,13 @@ if (Meteor.isServer){
 		return Events.find({});
 	});
 
+  Meteor.publish("myEvents", function(){
+    return Meteor.users.find({_id:Meteor.user()}, 
+      {fields: {'eventList':1}});
+  });
+
+  Meteor.publish("participant", function(){  });
+
   Accounts.onCreateUser(function(options, user){
     user.type = "Attendee";
     user.eventList = [];
@@ -16,34 +23,48 @@ if (Meteor.isServer){
   });
 }
 
+
 if (Meteor.isClient) {
 
 	Meteor.subscribe("directory");
 	Meteor.subscribe("Events");
-  // This code only runs on the client
+  Meteor.subscribe("myEvents");
+
+  Meteor.startup(function(){
+    Session.setDefault("templateName", "details");
+  });
 
   Template.body.helpers({
+    
+    templateName: function(){
+      return Session.get("templateName");
+    },
   	directory: function() {
   		return Meteor.users.find({});
   		//return Meteor.user().profile.username;
   	},
-    
-    events: function() {
-      return Events.find({});
+    myEvents: function() {
+      //Meteor.subscribe("personalEvents");
+      var user = Meteor.user();
+      return Events.find({_id: {$in: user.eventList}}).fetch();
     }
   });
 
-  Template.event.events({
-    "click .eventAdd": function() {
-      Meteor.call("eventAdd", this._id);
-      
-    },
-
+  Template.body.events({
+    "click .switchPage": function(){
+        if (Session.get("templateName") == "details") {
+          Session.set("templateName", "schedule");
+        }else{
+          Session.set("templateName", "details");
+        }
+      }
   });
 
-  Template.user.helpers({});
-
-  Template.details.helpers({});
+  Template.details.helpers({
+    events: function() {
+      return Events.find({});
+    },
+  });
 
   Template.details.events({
     "click .details": function(event) {
@@ -52,33 +73,56 @@ if (Meteor.isClient) {
       var typeSet = event.currentTarget.typeSel.value;
       console.log(typeSet);
       Meteor.call("changeType", typeSet);
-      
     }
-
-    /*
-    "click .type": function(event) {
-      //var type = $('input[name="type"]:checked', event.target.type.value)
-      event.preventDefault();
-      var typeSet = event.target.type.value;
-      Meteor.call("changeType", typeSet);
-      console.log(typeSet);
-    }*/
   });
+
+  Template.eventAdd.events({
+    "click .eventAdd": function() {
+      Meteor.call("eventAdd", this._id);
+    }
+  });
+
+  Template.schedule.helpers({
+    myEvents: function() {
+      //Meteor.subscribe("personalEvents");
+      var user = Meteor.user();
+      return Events.find({_id: {$in: user.eventList}}).fetch();
+    }
+  });
+
+  Template.schedule.events({});
+
+  Template.eventShow.helpers({
+    /*
+    personalEvents: function(){
+      var user = Meteor.user();
+      if(user && Array.isArray(user.eventList)){
+        return Events.find({_id: {$in: user.eventList }}). fetch();
+      }else{
+        return [];
+      }
+    },*/
+    participant: function() {
+      console.log (this._id);
+      eventId = this._id;
+      return Meteor.users.find(//{_id: !Meteor.user()._id} && 
+        {eventList: eventId }).fetch();
+    }
+  });
+
+  Template.user.helpers({});
 
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
-
 }
 
 Meteor.methods({
-
   changeType: function(typeSet){
     Meteor.users.update({_id:Meteor.user()._id}, {
       $set: {type : typeSet}
     });
   },
-
   eventAdd: function(eventId){
     console.log(eventId);
     Meteor.users.update({_id:Meteor.user()._id}, {
